7d60e2e08c5fd1dfd6750b28f29f40bd38acc828
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-02-24 17:49:08 +0000

    end

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,6 @@
 class Area < ApplicationRecord
   has_many :post_shops
   has_one :post_shop
-  validates :name, presence: true
+  # validates :name, presence: true
 
 end
--- app/models/shop.rb
+++
@@ -1,3 +0,0 @@
-class Shop < ApplicationRecord
-  has_one_attached :image
-end
