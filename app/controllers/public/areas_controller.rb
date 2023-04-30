00bd488ab44531960fd0ae7799cf2b1f0d25b197
EC2 Default User <ec2-user@ip-172-31-8-175.ap-northeast-1.compute.internal>
2023-04-30 15:14:45 +0000

    mypage/link

--- app/controllers/public/areas_controller.rb
+++ app/controllers/public/areas_controller.rb
@@ -2,12 +2,13 @@ class Public::AreasController < ApplicationController
   def index
 
     @post_shops = PostShop.all
+    # @post_shops = PostShop.page(params[:page])
     @areas = Area.all
 
-    
+
       @area = Area.find(params[:id])
       @post_shops = @area.post_shops
-    
+
   end
 
 
